f2389175a66bf76e07f6c9c1aeb1b368a562dc53
diazpedrodev <081diazpedro@gmail.com>
2022-10-22 13:08:05 -0300

    Curso/Mentoria

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso primeiroCurso = new Curso();
+        primeiroCurso.setTitulo(" Curso Java");
+        primeiroCurso.setDescricao(" Curso Basico voltado para iniciantes");
+        primeiroCurso.setCargaHoraria(8);
+
+        Curso segundoCurso = new Curso();
+        segundoCurso.setTitulo(" Curso Java POO");
+        segundoCurso.setDescricao(" Curso voltado para Oreientação a objeto");
+        segundoCurso.setCargaHoraria(12);
+
+        System.out.println(primeiroCurso);
+        System.out.println(segundoCurso);
+
+        Mentoria primeiraMentoria = new Mentoria();
+        primeiraMentoria.setTitulo(" Mentoria pro Curso Java");
+        primeiraMentoria.setDescricao(" Mentoria voltado para iniciantes em Java");
+        primeiraMentoria.setData(LocalDate.now());
+
+        Mentoria segundaMentoria = new Mentoria();
+        segundaMentoria.setTitulo(" Mentoria pro Curso Java POO");
+        segundaMentoria.setDescricao(" Mentoria voltado para Java POO");
+        segundaMentoria.setData(LocalDate.now());
+
+        System.out.println(primeiraMentoria);
+        System.out.println(segundaMentoria);
+    }
+}
